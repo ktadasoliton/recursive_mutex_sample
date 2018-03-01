a2d54e0bf71f6c7fd338fe78a3391d6a59f78596
ktada <kota.tada@soliton.co.jp>
2018-03-01 16:30:11 +0900

    refs #4603 一度に５つlockするようにした

--- include/rmutex/recursive_mutex.h
+++ include/rmutex/recursive_mutex.h
@@ -3,7 +3,12 @@
 #define RECURSIVEMUTEX_H_
 #include <mutex>
 
-void Work();
+void Work1();
+void Work2();
+void Work3();
+void Work4();
+void Work5();
 std::recursive_mutex& Mutex_();
 
 #endif  // RECURSIVEMUTEX_H_  //NOLINT
+
--- src/recursive_mutex.cpp
+++ src/recursive_mutex.cpp
@@ -1,22 +1,57 @@
 
   // Copyright 2018 Kota Tadaa
 #include <stdio.h>
+
+#ifdef _MSC_VER
 #include <Windows.h>
+#endif
+
 #include "rmutex/recursive_mutex.h"
 
 void main() {
   while (1) {
+#ifdef _MSC_VER
     Sleep(1000);
+#endif
+
     printf("main\n");
     Mutex_().lock();
-    Work();
+    Work1();
     Mutex_().unlock();
   }
 }
 
-void Work() {
+void Work1() {
+  Mutex_().lock();
+  printf("work1\n");
+  Work2();
+  Mutex_().unlock();
+}
+
+void Work2() {
+  Mutex_().lock();
+  printf("work2\n");
+  Work3();
+  Mutex_().unlock();
+}
+
+void Work3() {
+  Mutex_().lock();
+  printf("work3\n");
+  Work4();
+  Mutex_().unlock();
+}
+
+void Work4() {
+  Mutex_().lock();
+  printf("work4\n");
+  Work5();
+  Mutex_().unlock();
+}
+
+void Work5() {
   std::lock_guard<std::recursive_mutex> lock(Mutex_());
-  printf("work\n");
+  printf("work5\n");
 }
 
 std::recursive_mutex& Mutex_() {
